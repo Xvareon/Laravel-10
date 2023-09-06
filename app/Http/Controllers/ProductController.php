a3098a9e7afc6db0c9421a684beb782f649cde0b
Gavs <poOdrag0n69@gmail.com>
2023-09-06 16:10:07 +0800

    feat: Made a simple CRUD functionality

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products = Product::all();
+        return view('products.index', ['products' => $products]);
+    }
+
+    public function create(){
+        return view('products.create');
+    }
+
+    public function upload(Request $request){
+        /*
+        dd($request); // access the whole request
+        dd($request->variant); // access a specific data
+        */
+
+        $data = $request->validate([
+            'name' => 'required',
+            'variant' => 'required|numeric',
+            'qty' => 'required|numeric',
+            'price' => 'required|numeric',
+            'description' => 'required'
+        ]);
+
+        $newProduct = Product::create($data);
+
+        return redirect(route('product.index'))->with('success', 'Product Created Successfully!');
+    }
+
+    public function edit(Product $product){
+        return view('products.edit', ['product' => $product]);
+    }
+
+    public function update(Product $product, Request $request){
+        $data = $request->validate([
+            'name' => 'required',
+            'variant' => 'required|numeric',
+            'qty' => 'required|numeric',
+            'price' => 'required|numeric',
+            'description' => 'required'
+        ]);
+
+        $product->update($data);
+
+        return redirect(route('product.index'))->with('success', 'Product Updated Successfully!');
+    }
+
+    public function remove(Product $product){
+        $product->delete();
+
+        return redirect(route('product.index'))->with('success', 'Product Deleted Successfully!');
+    }
+}
---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" contents="ie=edge">
+    <title>Product Create Page</title>
+</head>
+
+<body>
+    <h1>Create a Product</h1>
+
+    <div>
+        <a href="{{route('product.index')}}">
+            Go back to index page
+        </a>
+    </div>
+
+    <div>
+        @if($errors->any())
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
+
+    <form method="post" action="{{route('product.upload')}}">
+        @csrf
+        @method('post')
+        <div>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Name" />
+        </div>
+        <div>
+            <label>Variant</label>
+            <input type="text" name="variant" placeholder="Variant" />
+        </div>
+        <div>
+            <label>Qty</label>
+            <input type="text" name="qty" placeholder="Qty" />
+        </div>
+        <div>
+            <label>Price</label>
+            <input type="text" name="price" placeholder="Price" />
+        </div>
+        <div>
+            <label>Description</label>
+            <input type="text" name="description" placeholder="Description" />
+        </div>
+        <div>
+            <input type="submit" value="Create the product">
+        </div>
+    </form>
+</body>
+
+</html>
